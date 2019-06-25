b7ede80b78efabf04f7d9f88a471042e4da74e50
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-06-25 13:20:16 -0400

    Update README and JSDoc

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const r = require("request")
 
 /**
  * Request comic image
- * @param {Object} options
+ * @param Object options
  * @param {Number[]} options.date - An array with date in the form [year,month,day]
  * @param {String} options.comicName - Name of comic strip \n e.g. gafield, bignate
- * @param {Boolean} options.urlOnly - If true, will return only the url of the image, not wrapped in a Request object
- * @returns {Promise} Promise object represents an instance of request() from the request library for the image
+ * @param {Boolean} options.URLOnly - If true, will return only the URL of the image, not wrapped in a Request object
+ * @returns {Promise|String} Promise object represents an instance of request() from the request library for the image, or String with image URL
  */
 exports.getImage = async function request(options) {
     const dateString = (function createForamttedDate() {
@@ -24,7 +24,7 @@ exports.getImage = async function request(options) {
         })
     )
 
-    const imageUrl = parsedPage.querySelector(".item-comic-image img").rawAttrs.split(/ src=/)[1].replace(/"/g, "");
+    const imageURL = parsedPage.querySelector(".item-comic-image img").rawAttrs.split(/ src=/)[1].replace(/"/g, "");
 
-    return options.urlOnly ? imageUrl : r(imageUrl);
+    return options.URLOnly ? imageURL : r(imageURL);
 }
